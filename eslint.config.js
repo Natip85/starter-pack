b88b1d91f57df6687d3579bff203ed3f996c3f88
Natip85 <106977567+Natip85@users.noreply.github.com>
2025-05-17 12:57:10 +0300

    readme fix4

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default tseslint.config(
     ],
     rules: {
       "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/consistent-type-imports": [
         "warn",
